903ee1f8aa00a5ce2d90d08db97659cb7ea77dfb
Jordan Ford <jsford94@gmail.com>
2021-06-03 10:24:20 -0400

    Add swiftnav GPS driver for the piksi RTK GPS module.

---
+++ src/swiftnav_ros/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/pitranger/PitROS/src/localization/swiftnav_ros/include".split(';') if "/home/pitranger/PitROS/src/localization/swiftnav_ros/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "diagnostic_updater;roscpp;sensor_msgs;std_srvs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lsbp_device;-lswiftnav_ros_driver".split(';') if "-lsbp_device;-lswiftnav_ros_driver" != "" else []
+PROJECT_NAME = "swiftnav_ros"
+PROJECT_SPACE_DIR = "/home/pitranger/PitROS/src/localization/swiftnav_ros/build/devel"
+PROJECT_VERSION = "0.0.1"
