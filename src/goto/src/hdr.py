7dedebd171e20b4421c93bee5f4c7d4127406361
Jordan Ford <jsford94@gmail.com>
2021-08-08 00:07:35 -0400

    Add code to merge bracketed image sets into tonemapped HDR images.

---
+++ src/goto/src/hdr.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+
+def hdr_merge(images, exposures, gamma=1.8):
+    '''Merge multiple exposure-bracketed images into a single tonemapped image.'''
+    # Remove gamma from the starting images.
+    # NOTE(Jordan): Pitcam applies a 1.8 gamma to its images.
+    images = [_adjust_gamma(im, gamma) for im in images]
+    # Align the images and crop them to the overlapping region.
+    images, exposures = _align_image_bracket(images, exposures)
+    # Merge the images.
+    hdr_image = _merge_images_mertens(images, exposures)
+    return hdr_image
+
+def _align_images_sift(im1, im2):
+  '''Align im1 to im2. Warp im1 to match im2.
+  Return the warped im1 and the homography h that was used to warp it.'''
+
+  # Convert images to grayscale
+  im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+  im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+  # Initiate SIFT detector
+  sift = cv2.xfeatures2d.SIFT_create()
+
+  # find the keypoints and descriptors with SIFT
+  kp1, des1 = sift.detectAndCompute(im1Gray,None)
+  kp2, des2 = sift.detectAndCompute(im2Gray,None)
+
+  # Matching descriptor vectors with a FLANN based matcher
+  matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+  matches = matcher.knnMatch(des1, des2, 2)
+
+  # Apply ratio test
+  good = []
+  for m,n in matches:
+      if m.distance < 0.7*n.distance:
+          good.append([m])
+
+  # Extract location of good matches
+  points1 = np.array([kp1[g[0].queryIdx].pt for g in good])
+  points2 = np.array([kp2[g[0].trainIdx].pt for g in good])
+
+  # Find homography
+  h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+  # Use homography
+  height, width, channels = im2.shape
+  im1Reg = cv2.warpPerspective(im1, h, (width, height))
+
+  return im1Reg, h
+
+
+def _align_image_bracket(images, exposures):
+    '''Sort images by increasing exposure. Align each image to the center image
+    in the exposure bracket. Warp and crop all images to cover only the overlapping area.'''
+
+    assert len(images) == len(exposures), 'Please provide one exposure value per image.'
+
+    # N is the number of images to be processed.
+    N = len(images)
+
+    # Sort images by increasing exposure.
+    image_list = [(images[i], exposures[i]) for i in range(N)]
+    image_list.sort(key=lambda e: e[1], reverse=True)
+
+    # Each image will be matched against the image in the center of the exposure bracket.
+    base_image = image_list[N/2][0]
+    base_exposure = image_list[N/2][1]
+    
+    # Match images.
+    aligned_images = []
+    aligned_image_exposures = []
+    aligned_image_homographies = []
+    for i in range(N):
+        curr_image = image_list[i][0]
+        curr_exposure = image_list[i][1]
+
+        # Don't match the center image to itself.
+        if i == N/2:
+            reg_image = curr_image
+            H = np.identity(3)
+        else:
+            base_image = base_image.astype('uint8')
+            reg_image, H = _align_images_sift(curr_image, base_image)
+        aligned_images.append(reg_image)
+        aligned_image_exposures.append(curr_exposure)
+        aligned_image_homographies.append(H)
+
+    # Find the bounds of the overlapping area shared by all images.
+    bound_x = [0, base_image.shape[1]]
+    bound_y = [0, base_image.shape[0]]
+    for h in aligned_image_homographies:
+        cx = h[0,2]
+        cy = h[1,2]
+        bound_x[0] = max(bound_x[0], bound_x[0]+cx)
+        bound_x[1] = min(bound_x[1], bound_x[1]+cx)
+        bound_y[0] = max(bound_y[0], bound_y[0]+cy)
+        bound_y[1] = min(bound_y[1], bound_y[1]+cy)
+    bound_x = [int(x) for x in bound_x]
+    bound_y = [int(y) for y in bound_y]
+
+    # Crop all images to only the overlap.
+    aligned_images = [img[bound_y[0]:bound_y[1], bound_x[0]:bound_x[1]] for img in aligned_images]
+
+    return aligned_images, aligned_image_exposures
+
+def _merge_images_mertens(images, exposures):
+    # Exposure fusion using Mertens
+    merge_mertens = cv2.createMergeMertens()
+    res_mertens = merge_mertens.process(images)
+
+    res_mertens_8bit = np.clip(res_mertens*255, 0, 255).astype('uint8')
+    res_mertens_8bit = _adjust_gamma(res_mertens_8bit, 1.0/2.2)
+    return res_mertens_8bit
+
+def _adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+            for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+if __name__=="__main__":
+    image_paths = ['images/ae_-2.jpg', 'images/ae_+0.jpg', 'images/ae_+2.jpg']
+    exposures = [5500.0, 11000.0, 44000.0]
+
+    images = [cv2.imread(p) for p in image_paths]
+    hdr_image = hdr_merge(images, exposures)
+    cv2.imwrite('hdr_merge.jpg', hdr_image)
